6dc800bc587dc6e0b9e3a0bbe25154051b9a6f9b
MohammadFahad2606 <design4youonline@gmail.com>
2024-02-12 13:21:36 +0500

    instagram login page added

--- css/instagram/script.js
+++ css/instagram/script.js
@@ -1,7 +1,7 @@
-alert("Thanks for your input!");
-alert("number2 + number1 =")
+// alert("Thanks for your input!");
+// alert("number2 + number1 =")
 
-var number1 =5
-var number2 =10
+// var number1 =5
+// var number2 =10
 
 
