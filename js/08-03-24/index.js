732503bc5d7b4cd15b0e1036cc7944de7a7a0b14
MohammadFahad2606 <design4youonline@gmail.com>
2024-03-26 20:15:45 +0500

    Portfolio site edit and js class many files add

---
+++ js/08-03-24/index.js
@@ -0,0 +1,5 @@
+alert("Hello JS");
+setInterval(() => {
+  let heading = document.getElementById("heading");
+  heading.innerText = "New";
+}, 1000);
---
+++ js/16-03-24/Product page.js
@@ -0,0 +1,23 @@
+// single Product page
+
+// let header = document.getElementById("header");
+
+// window.location.href(newUrl);
+let containerPro = document.getElementById("containerPro");
+console.log(containerPro.innerHTML);
+
+
+
+<div class="product-details">
+<img src="https://i.ebayimg.com/images/g/NzoAAOSw9hpl8xr2/s-l960.jpg" alt="Product Name" />
+<div class="info">
+  <h2>Product Name</h2>
+  <p class="price">$20.00</p>
+  <p class="description">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+    metus justo. Nullam fermentum magna ut urna fermentum, ut placerat
+    lorem eleifend.
+  </p>
+  <button>Add to Cart</button>
+</div>
+</div>
