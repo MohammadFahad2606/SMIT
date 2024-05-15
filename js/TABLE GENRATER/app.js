d8c9bd9df67ca134bc5fc44b1dfe3adc848f94fe
MohammadFahad2606 <design4youonline@gmail.com>
2024-05-15 14:05:32 +0500

    table genrater edit

--- js/TABLE GENRATER/app.js
+++ js/TABLE GENRATER/app.js
@@ -1,19 +1,22 @@
 function userBtn() {
-  let tableLength = document.querySelector("#tableLength").value;
-  let tableNumber = document.querySelector("#tableNumber").value;
+  let tableLength = document.querySelector("#tableLength");
+  let tableNumber = document.querySelector("#tableNumber");
   let containerBox = document.querySelector("#containerBox");
 
-  if (tableLength !== "" && tableLength !== "") {
-    for (let i = 1; i <= tableLength; i++) {
-      containerBox.innerHTML += `<h2><span>${tableNumber}</span> <span>x</span> <span>${i}</span> <span>=</span><span>${
-        tableNumber * tableLength
+  if (tableLength.value !== "" && tableLength.value !== "") {
+    for (let i = 1; i <= tableLength.value; i++) {
+      containerBox.innerHTML += `<h2><span>${
+        tableNumber.value
+      }</span> <span>x</span> <span>${i}</span> <span>=</span><span>${
+        tableNumber.value * i
       }</span></h2>`;
     }
   } else {
     Swal.fire("Please Enter Table Number");
   }
-  console.log(tableLength);
-  console.log(tableNumber);
-  tableLength = "";
-  tableNumber = "";
+
+  console.log(tableLength.value);
+  console.log(tableNumber.value);
+  tableLength.value = "";
+  tableNumber.value = "";
 }
