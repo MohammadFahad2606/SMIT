3de4b7c9c2a74e64357d3770c5efea7203905935
MohammadFahad2606 <design4youonline@gmail.com>
2024-05-17 12:34:35 +0500

    PARAGRAPH CHECKER done

--- js/Paragraph checker/app.js
+++ js/Paragraph checker/app.js
@@ -1,5 +1,21 @@
 let userInput = document.querySelector("#userInput");
+let boxwrap = document.querySelector("#boxwrap");
+let words = document.querySelector("#words");
+let letters = document.querySelector("#letters");
+let lines = document.querySelector("#lines");
+let parag = document.querySelector("#paragraph");
+console.log(parag.innerText);
 
 function clickBtn() {
-  console.log(userInput.value);
+  boxwrap.style = "display: flex;";
+  let wods = userInput.value.length;
+  let leter = userInput.value.split(" ");
+  let line = userInput.value.split(".");
+  let pra = userInput.value.split("\n");
+  console.log(pra.length);
+  words.innerText = wods;
+  letters.innerText = leter.length;
+  letters.innerText = leter.length;
+  parag.innerHTML = pra.length;
+  // console.log(ck);
 }
