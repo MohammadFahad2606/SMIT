212360b14d7d701c611824e4da1c5a3c6f896262
MohammadFahad2606 <design4youonline@gmail.com>
2024-07-28 15:34:50 +0500

    weather app ui done not responsiv only desktop

--- js/Weather App/app.js
+++ js/Weather App/app.js
@@ -1,28 +1,41 @@
-const apicall = () => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-`);
-};
+let cityname = document.querySelector("#search");
+let searchBtn = document.querySelector("#searchbtn");
+let apikey =
+searchBtn.addEventListener("click", fetchdata);
+function fetchdata() {
+ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname.value}&appid=${apikey}`)
 
-// function getCurrentLocation() {
-//   navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//       getCruntDataApi(position.coords.latitude, position.coords.longitude);
-//     },
-//     (error) => {
-//       alert(error.message);
-//     }
-//   );
-// }
-// getCurrentLocation();
 
-async function getCruntDataApi(lat, lon) {
-  try {
-    let data = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fb763078c8af7fcd748f1e4f15057d46`
-    );
-    let res = await data.json();
-    console.log(res);
-  } catch (err) {
-    console.log(err);
-  }
+
+
+
 }
+
+// const apicall = () => {
+//   fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// `);
+// };
+
+// // function getCurrentLocation() {
+// //   navigator.geolocation.getCurrentPosition(
+// //     (position) => {
+// //       getCruntDataApi(position.coords.latitude, position.coords.longitude);
+// //     },
+// //     (error) => {
+// //       alert(error.message);
+// //     }
+// //   );
+// // }
+// // getCurrentLocation();
+
+// async function getCruntDataApi(lat, lon) {
+//   try {
+//     let data = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fb763078c8af7fcd748f1e4f15057d46`
+//     );
+//     let res = await data.json();
+//     console.log(res);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
